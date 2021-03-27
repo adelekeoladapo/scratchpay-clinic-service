6c9b55a57a171fab3a6a134f15e652e361ef5d4a
Dapo Adeleke <adelekeoladapo@gmail.com>
2021-03-27 18:13:04 +0100

    Imported Echo framework

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module scratchpay.com/clinics
 
 go 1.14
+
+require github.com/labstack/echo/v4 v4.2.1 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	"fmt"
+	"net/http"
 )
 
 func main() {
+	e := echo.New()
 	fmt.Println("Hello ScratchPay!")
+
+	/* App Router */
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello ScratchPay")
+	})
+	e.Logger.Fatal(e.Start(":8090"))
 }
